887ba7dd4e88b4e6280546d8cb308b2d74f6c58b
Freddy Wang <fcwang@mail.smu.edu>
2020-03-05 21:48:14 -0600

    remove tests for now

--- cmake-build-debug/CMakeFiles/Sorting.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/Sorting.dir/cmake_clean.cmake
@@ -2,8 +2,6 @@ file(REMOVE_RECURSE
   "CMakeFiles/Sorting.dir/DSString.cpp.o"
   "CMakeFiles/Sorting.dir/Sorter.cpp.o"
   "CMakeFiles/Sorting.dir/main.cpp.o"
-  "CMakeFiles/Sorting.dir/stringTests.cpp.o"
-  "CMakeFiles/Sorting.dir/vectorTests.cpp.o"
   "Sorting"
   "Sorting.pdb"
 )
