d68464931dab1f8d9a49cb6fd761b9ad3831c670
Frederick C. Wang <54632396+FreddyWang-12@users.noreply.github.com>
2020-03-13 22:21:43 -0500

    Update Sorter.cpp

--- Sorter.cpp
+++ Sorter.cpp
@@ -10,12 +10,14 @@
 #include "stdio.h"
 using namespace std;
 
+// Default constructor of the Sorter Class
 Sorter::Sorter(){
     length = 0;
     wordLengthCheckpoints = nullptr;
     allWords = nullptr;
 }
-
+// Reads a text file and adds all of the words inside a char** array
+// @param inputFile: The file being read
 void Sorter::readWordFile(ifstream& inputFile){
     if(inputFile.is_open()){
         char number[15];
@@ -33,7 +35,8 @@ void Sorter::readWordFile(ifstream& inputFile){
 }
 
 
-
+// @param start: The starting index of the sort
+// @param end: The last index of the sort
 void Sorter::sortWordsByWordLength(int start, int end){
     if(allWords != nullptr) {
         if (start < end) {
@@ -46,7 +49,9 @@ void Sorter::sortWordsByWordLength(int start, int end){
         maxWordLength = strlen(allWords[length - 1]);
     }
 }
-
+// Partitions a section of the list by word length
+// @param start: The starting index of the partition
+// @param end: The last index of the partion
 int Sorter::partitionByWordLength(int start, int end) {
     int i = start - 1;
     for(int j = start; j < end; j++) {
@@ -61,7 +66,12 @@ int Sorter::partitionByWordLength(int start, int end) {
 }
 
 
-
+// Sorts a section of the list alphabetically based on word length
+// Each part of the list will be sectioned off by checkpoints that 
+// mark the end of a section of the list. Each section is based on 
+// word length. It is this method's job to sort a individual section.
+// @param start: The starting index of the sort
+// @param end: The last index of the sort
 void Sorter::sortWordSectionAlpha(int start, int end){
     if(allWords != nullptr) {
         if (start < end) {
@@ -73,7 +83,10 @@ void Sorter::sortWordSectionAlpha(int start, int end){
         }
     }
 }
-
+// Takes through all sections of the list (after being sorted by word length)
+// and sorts them all out alphabetically. These sections are determined by their
+// word length, so all 1-letter words will be sorted individually,all 2-letter 
+// words will be sorted individually, etc.
 void Sorter::sortAllWordsAlphabetically() {
     sortWordSectionAlpha(beginning, wordLengthCheckpoints[0]);
     for(int i = 0; i < maxWordLength - 1; i++){
@@ -84,7 +97,9 @@ void Sorter::sortAllWordsAlphabetically() {
     int ending = length - 1;
     sortWordSectionAlpha(finalIndex, ending);
 }
-
+// Partitions a section of the list via an alphabetical quicksort
+// @param start: The starting index of the partition
+// @param end: The last index of the partion
 int Sorter::partitionAlphabetically(int start, int end) {
     int i = start - 1;
     for(int j = start; j < end; j++){
@@ -97,7 +112,10 @@ int Sorter::partitionAlphabetically(int start, int end) {
     swap(allWords[i], allWords[end]);
     return i;
 }
-
+// Each part of the list will be sectioned off by checkpoints that 
+// mark the end of a section of the list. Each section is based on 
+// word length. It is this method's job to sort create those 
+// checkpoints that will define those sections.
 void Sorter::createAlphabeticalCheckpoints() {
     if(wordLengthCheckpoints != nullptr) {
         delete[] wordLengthCheckpoints;
@@ -115,7 +133,8 @@ void Sorter::createAlphabeticalCheckpoints() {
 }
 
 
-
+// Swaps the values of 2 of the elements inside allWords
+// @param i, j: The two elements being swapped
 void Sorter::swap(char *&i, char*&j) {
     char * temp = i;
     i = j;
@@ -123,14 +142,15 @@ void Sorter::swap(char *&i, char*&j) {
 }
 
 
-
+// Creates an output file of all of the contents of the word list
+// @param outputFile: The name of the output file
 void Sorter::printSortedWordFile(ofstream& outputFile){
     outputFile << length << endl;
     for(int i = 0; i < length; i++){
         outputFile << allWords[i] << endl;
     }
 }
-
+// Returns the length of the word list
 int& Sorter::getListLength() {
     return length;
 }
